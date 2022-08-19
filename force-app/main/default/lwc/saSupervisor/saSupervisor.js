bec077a7a91fa82ecb0efd6df6aa7600f70772a1
Ercan <234ercan@gmail.com>
2022-08-19 15:16:26 +0100

    second

--- force-app/main/default/lwc/saSupervisor/saSupervisor.js
+++ force-app/main/default/lwc/saSupervisor/saSupervisor.js
@@ -1,7 +1,7 @@
 import { LightningElement, api, wire } from 'lwc';
 import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
 
-import SUPERVISOR_FIELD from '@salesforce/schema/sa_details__c.Contract__c';
+import SUPERVISOR_FIELD from '@salesforce/schema/sa_details__c.Supervisor__c';
 const saFields = [SUPERVISOR_FIELD];
 export default class SASupervisor extends LightningElement {
 	@api recordId; 
